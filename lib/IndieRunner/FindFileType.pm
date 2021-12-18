7ce749db55f850ff259a652d92fb4918da3ed0b0
thfrwn <11335318+rfht@users.noreply.github.com>
2021-12-18 09:29:07 -0700

    reformat for tab width 8

--- lib/IndieRunner/FindFileType.pm
+++ lib/IndieRunner/FindFileType.pm
@@ -7,23 +7,23 @@ use version; our $VERSION = qv('0.1.0');
 use File::Find::Rule;
 use File::LibMagic;
 
-our %filetypes = (	# allowed filetype strings with filename patterns to look at when _fast
-	'Config'				=> [ '*.config', ],
-	'Data'					=> [ '*' ],				# could be anything
-	'ManagedEntryPoint'		=> [
-							     '*.exe',			# e.g. RogueLegacy.exe
-							     '*boot.dat',		# for hashlink
-							   ],
-	'ManagedFramework'		=> [
-							     'FNA.dll',
-							     'MonoGame.Framework.dll',
-							   ],
-	'ManagedLibrary'		=> [ '*.dll', ],	# e.g. Steamworks.NET.dll
-	'NativeExecutable'		=> [ '*' ],			# e.g. RogueLegacy.bin.x86_64
-	'NativeLibrary'			=> [
-							     '*.so*',		# e.g. libSDL2-2.0.so.0, 
-							     '*.hdll',		# e.g. openal.hdll
-							   ],
+our %filetypes = (
+	'Config'		=> [ '*.config', ],
+	'Data'			=> [ '*' ],
+	'ManagedEntryPoint'	=> [
+					'*.exe',	# e.g. RogueLegacy.exe
+					'*boot.dat',	# for hashlink
+				   ],
+	'ManagedFramework'	=> [
+					'FNA.dll',
+					'MonoGame.Framework.dll',
+				   ],
+	'ManagedLibrary'	=> [ '*.dll', ],	# e.g. Steamworks.NET.dll
+	'NativeExecutable'	=> [ '*' ],		# e.g. RogueLegacy.bin.x86_64
+	'NativeLibrary'		=> [
+					'*.so*',	# e.g. libSDL2-2.0.so.0, 
+					'*.hdll',	# e.g. openal.hdll
+				   ],
 );
 
 # go through all files and check with LibMagic for a match
@@ -31,27 +31,33 @@ sub find_file_type {
 	my $directory	= $_[0];
 	my $type	= $_[1];
 	my $fast	= $_[2] || 0;	# 1: speed up by matching filenames
+	my $file;
 	my @file_list;
 	my @out_list;
 
 	unless (exists($filetypes{$type})) { die "Error: Invalid filetype"; }
 
-	if $fast {
-		@file_list = File::Find::Rule->file->nonempty->name( $filetypes{$type} )->$in( $directory );
-	} else {
-		@file_list = File::Find::Rule->file->nonempty->in( $directory );
+	if ($fast) {
+		@file_list = File::Find::Rule->file
+					     ->nonempty
+					     ->name( $filetypes{$type} )
+					     ->in( $directory );
 	}
-	foreach my $file (@file_list) {
-		#print File::LibMagic->new->info_from_filename( $file )->{description}
-		    #. "\n";
-		if ( index( File::LibMagic->new->info_from_filename( $file )->{description},
+	else {
+		@file_list = File::Find::Rule->file
+					     ->nonempty
+					     ->in( $directory );
+	}
+	foreach $file (@file_list) {
+		if ( index( File::LibMagic->new
+					  ->info_from_filename( $file )
+					  ->{description},
 		     'Mono/.Net assembly' ) > -1 ) {
 			push @out_list, $file;
 		}
 	}
 	print join("\n", @out_list) . "\n";
 	
-
 	return @out_list;
 }
 
