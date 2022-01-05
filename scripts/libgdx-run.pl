240996f59515ccf9aa6d13f3f773163122ae03cb
thfrwn <11335318+rfht@users.noreply.github.com>
2022-01-05 07:46:46 -0700

    complete the execution as tested with Urtuk

--- scripts/libgdx-run.pl
+++ scripts/libgdx-run.pl
@@ -14,26 +14,10 @@ use Readonly;
 use IndieRunner::IndieRunner;
 
 Readonly::Scalar my $CONFIG_FILE	=> 'config.json';
-
 # version string examples: '1.8.0_312-b07', '1.8.0_181-b02', '11.0.13+8-1', 17.0.1+12-1
 Readonly::Scalar my $JAVA_VER_REGEX	=> '\d{1,2}\.\d{1,2}\.\d{1,2}[_\+][\w\-]+';
 
-my $Work_Dir;
-
 my $Os;	
-my $Os_Java_Version;	# e.g. '1.8.0'
-my $Os_Java_Home;
-my $Os_Java_Bin;	# e.g. '/usr/local/jdk-1.8.0/bin/java'
-my $Jar_Bin;		# e.g. '/usr/local/jdk-1.8.0/bin/jar'
-my @Class_Path;		# from $CONFIG_FILE, e.g. [ 'desktop-1.0.jar', ]
-my $Main_Class;		# from $CONFIG_FILE, e.g. 'kaleta.hex3.desktop.DesktopLauncher'
-my @Jvm_Args;		# from $CONFIG_FILE, e.g. [ '-Xmx1G', '-Xms1G', ]
-my @Jvm_Env;		# JAVA_HOME, PATH, JAVACMD
-
-my $Bundled_Java_Bin;	# bundled (Linux) JRE binary containing $Java_Ver
-
-my $Config_Data;	# converted content from $CONFIG_FILE
-my @System_Command;	# the command to be executed with all parameters as array
 
 my %Valid_Java_Versions = (
 	'openbsd'	=> [
@@ -44,57 +28,95 @@ my %Valid_Java_Versions = (
 );
 
 sub get_java_version {
-	$Bundled_Java_Bin = 'jre/bin/java';	# alternatives:	jre/lib/amd64/libjava.so
-						#		jre/lib/amd64/libjvm.so`
+	my $bundled_java_bin;
+	my $os_java_version;
 
-	# fetch version string from the $Bundled_Java_Bin
-	my $java_bin_content = path($Bundled_Java_Bin)->slurp_raw;
+	# find bundled java binary, alternatively libjava.so or libjvm.so
+	# TODO: make smarter
+	$bundled_java_bin = 'jre/bin/java';
+
+	# fetch version string from the $bundled_java_bin
+	my $java_bin_content = path($bundled_java_bin)->slurp_raw;
 	my $got_version = $1 if ($java_bin_content =~ /($JAVA_VER_REGEX)/);
 
 	# trim $version_str string to OS JAVA_HOME
 	if ( $Os eq 'openbsd' ) {
 		# OpenBSD: '1.8.0', '11', '17'
 		if (substr($got_version, 0, 2) eq '1.') {
-			$Os_Java_Version = '1.8.0';
+			$os_java_version = '1.8.0';
 		}
 		else {
-			$Os_Java_Version = $got_version =~ /^\d{2}/;
+			$os_java_version = $got_version =~ /^\d{2}/;
 		}
 	}
 	else {
 		die "Unsupported OS: $Os";
 	}
 
-	# validate $Os_Java_Version
-	unless (grep( /^$Os_Java_Version$/, @{$Valid_Java_Versions{$Os}} )) {
-		die ( "No valid Java version found in '$Bundled_Java_Bin': ",
-			"$Os_Java_Version"
+	# validate $os_java_version
+	unless (grep( /^$os_java_version$/, @{$Valid_Java_Versions{$Os}} )) {
+		die ( "No valid Java version found in '$bundled_java_bin': ",
+			"$os_java_version"
 		    );
 	}
 
-	return $Os_Java_Version;
+	return $os_java_version;
+}
+
+sub get_java_home {
+	my $java_home;
+
+	if ( $Os eq 'openbsd' ) {
+		$java_home = '/usr/local/jdk-' . get_java_version;
+	}
+	else {
+		die "Unsupported OS: $Os";
+	}
+
+	-d $java_home ? return $java_home :
+		die "failed to get JAVA_HOME directory at $java_home: $!";
 }
 
 sub run {
+	my $config_data;
+	my $qx_string;
+	my $java_home;
+	my $main_class;
+	my @class_path;
+	my @jvm_env;
+	my @jvm_args;
+	my @system_args;
 
-	# get OS
+	# get OS and OS Java variables
 	$Os = IndieRunner::IndieRunner::detectplatform;
 	unless ( exists $Valid_Java_Versions{$Os} ) {
 		die "OS not recognized: $Os";
 	}
+	$java_home = get_java_home;
 
 	# slurp and assign config data
-	$Config_Data		= decode_json(path($CONFIG_FILE)->slurp_utf8)
+	$config_data		= decode_json(path($CONFIG_FILE)->slurp_utf8)
 		or die "unable to read config data from $CONFIG_FILE: $!";
-	@Class_Path		= $$Config_Data{'classPath'}
-		or die "Unable to get configuration for classPath: $!";
-	$Main_Class		= $$Config_Data{'mainClass'}
+	$main_class		= $$config_data{'mainClass'}
 		or die "Unable to get configurarion for mainClass: $!";
-	if ( exists($$Config_Data{'vmArgs'}) ) {
-		@Jvm_Args	= $$Config_Data{'vmArgs'}
+	@class_path		= $$config_data{'classPath'}
+		or die "Unable to get configuration for classPath: $!";
+	if ( exists($$config_data{'vmArgs'}) ) {
+		@jvm_args	= @{$$config_data{'vmArgs'}};
 	}
 
-	print Dumper($Os, @Class_Path, $Main_Class, @Jvm_Args);
+	# build command and execute
+	# TODO: call JVM directly via PBJ::JNI or Jvm from CPAN; better portability?
+	@jvm_env	= (
+				"JAVA_HOME=$java_home",
+				"PATH=$java_home/bin:\$PATH"
+			  );
+	@system_args = ( 'env', @jvm_env, 'java', @jvm_args, $main_class );
+	say "\nExecuting Jave Virtual Machine:";
+	say join( ' ', @system_args ) . "\n";
+	$qx_string = join( ' ', ( @system_args, '2>&1 > indierun.log' ) );
+	qx($qx_string);
+	say "Execution completed with exit code " . ($? >> 8);
 }
 
 run;
