a733f3a8e901178a8bf242673c626baafe3431c1
thfrwn <11335318+rfht@users.noreply.github.com>
2024-05-21 21:49:35 -0400

    Engine.pm: add POD

--- lib/IndieRunner/Engine.pm
+++ lib/IndieRunner/Engine.pm
@@ -13,13 +13,42 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 package IndieRunner::Engine;
+
+=head1 NAME
+
+IndieRunner::Engine - supported game engines
+
+=cut
+
 use v5.36;
 use version 0.77; our $VERSION = version->declare('v0.0.1');
+
+=head1 DESCRIPTION
+
+B<Warning! Do not use this class directly, as it is a prototype class for specific engines!>
+
+Parent class for specific IndieRunner engine modules. Refer to specific engine modules under L</SEE ALSO>.
+
+=head1 METHODS
+
+=cut
+
+=head2 new()
+
+Create new engine object.
+
+=cut
 
 sub new ( $class, %args ) {
 	my $self = bless { %args }, $class;
 	return $self;
 }
+
+=head2 setup()
+
+Perform engine-specific setup operations.
+
+=cut
 
 sub setup ( $self ) {
 	$$self{ mode_obj }->vvsay( 'Setup' );
@@ -30,21 +59,72 @@ sub setup ( $self ) {
 	}
 }
 
+=head2 get_bin()
+
+Return the binary to use for the engine.
+
+=cut
+
 sub get_bin( $self ) {
 	die "not implemented for $self: " . (caller(0))[3];
 }
+
+=head2 get_env_ref()
+
+Return the environment settings for the engine execution.
+
+=cut
 
 sub get_env_ref( $self ) {
 	return \@;
 }
 
+
+=head2 get_args_ref()
+
+Return the arguments to the engine for execution.
+
+=cut
+
 sub get_args_ref( $self ) {
 	return \@;
 }
 
-# game_name needed for heuristics to identify the main file to execute
+=head2 set_game_name($name)
+
+Set the game's name, to be used by engine-specific heuristics for how to launch the game.
+
+=cut
+
 sub set_game_name ( $self, $name ) {
 	$$self{ game_name } = $name;
 }
 
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<IndieRunner::Engine::FNA>
+L<IndieRunner::Engine::GZDoom>
+L<IndieRunner::Engine::Godot>
+L<IndieRunner::Engine::HashLink>
+L<IndieRunner::Engine::Java>
+L<IndieRunner::Engine::Love2D>
+L<IndieRunner::Engine::Mono>
+L<IndieRunner::Engine::MonoGame>
+L<IndieRunner::Engine::ScummVM>
+L<IndieRunner::Engine::XNA>
+
+=head1 AUTHOR
+
+Thomas Frohwein E<lt>thfr@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2022-2024 by Thomas Frohwein E<lt>thfr@cpan.orgE<gt>.
+
+This program is free software; you can redistribute it and/or modify it under the ISC license.
+
+=cut
