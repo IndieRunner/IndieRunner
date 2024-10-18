26ffd3ec5412097602f6d63917ad76a98eb67445
thfrwn <11335318+rfht@users.noreply.github.com>
2024-10-18 00:19:13 -0400

    use ARGV0_SYMLINK (.godot-indierunner) as a hack for ARGV0 that works around issues of games expecting the binary to be bundled with the game. Issue noticed with Crossroad OS that was looking for a save file location adjacent to the binary, and then with Brotato that reads Steamworks info from a bundled file. Fixed with this symlink approach.

--- lib/IndieRunner/Engine/Godot.pm
+++ lib/IndieRunner/Engine/Godot.pm
@@ -13,6 +13,13 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 package IndieRunner::Engine::Godot;
+
+=head1 NAME
+
+IndieRunner::Engine::Godot - Godot engine module
+
+=cut
+
 use v5.36;
 use version 0.77; our $VERSION = version->declare('v0.0.1');
 
@@ -21,17 +28,39 @@ use parent 'IndieRunner::Engine';
 use Carp;
 use IndieRunner::Helpers qw( match_bin_file );
 
+=head1 DESCRIPTION
+
+Module to set up and launch games made with the Godot game engine.
+Currently supports games made with Godot version 3.x or 4.x.
+
+=head2 Status
+
+Workarounds:
+
+=over 8
+
+=item Symlink hack to simulate the use of bundled Godot binary (ARGV0_SYMLINK)
+
+=back
+
+Limitations on OpenBSD include:
+
+=over 8
+
+=item Missing support for some GDNative/GDExtension modules
+
+=item No support for encrypted Godot games
+
+=back
+
+=cut
+
 use constant GODOT3_BIN	=>		'/usr/local/bin/godot';
 use constant GODOT4_BIN =>		'/usr/local/bin/godot4';
 use constant PACK_HEADER_MAGIC =>	'GDPC';
+use constant ARGV0_SYMLINK =>		'.godot-indierunner';
 
 my $game_file;
-
-###
-# Godot 4 games for future addition when implemented:
-# - Buckshot Roulette (itch.io)
-# - 8 Colors Star Guardians (itch.io)
-###
 
 sub get_pack_format_version( $file ) {
 	# [\x00-\x02] - marker for pack version for Godot 2 (\x00) to
@@ -51,18 +80,23 @@ sub get_bin( $self ) {
 	$game_file = $$self{ id_file } if $$self{ id_file };
 	$game_file = ( glob( '*.pck' ) )[0] unless $game_file;
 	my $pack_format_version = get_pack_format_version( $game_file );
+
+	# XXX: create local symlink, fixes issues with binary location (some software like
+	#      Crossroad OS expects binary to be in the game dir)
+
 	if ( $pack_format_version == 0 ) {
 		die "No runtime for Godot version 2 (pack version 0 in $game_file)";
 	}
 	elsif ( $pack_format_version == 1 ) {
-		return GODOT3_BIN;
+		$$self{ mode_obj }->insert( GODOT3_BIN, ARGV0_SYMLINK );
 	}
 	elsif ( $pack_format_version == 2 ) {
-		return GODOT4_BIN;
+		$$self{ mode_obj }->insert( GODOT4_BIN, ARGV0_SYMLINK );
 	}
 	else {
 		croak "unable to determine Godot binary from $game_file";
 	}
+	return './' . ARGV0_SYMLINK;
 }
 
 sub get_args_ref( $self ) {
@@ -75,3 +109,11 @@ sub get_args_ref( $self ) {
 }
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Thomas Frohwein E<lt>thfr@cpan.orgE<gt>.
+
+This program is free software; you can redistribute it and/or modify it under the ISC license.
