257e4efbf2997adff04086ee59b808b17611f20a
thfrwn <11335318+rfht@users.noreply.github.com>
2025-12-14 18:57:29 -0800

    add POD to JavaMod.pm

--- lib/IndieRunner/Engine/Java/JavaMod.pm
+++ lib/IndieRunner/Engine/Java/JavaMod.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2024 Thomas Frohwein
+# Copyright (c) 2022-2025 Thomas Frohwein
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -13,6 +13,19 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 package IndieRunner::Engine::Java::JavaMod;
+
+=head1 NAME
+
+IndieRunner::Engine::Java::JavaMod - template for modules used by L<IndieRunner::Engine::Java>
+
+=head1 DESCRIPTION
+
+Basic variables and methods for modules used by L<IndieRunner::Engine::Java>.
+
+=over 8
+
+=cut
+
 use v5.36;
 use version 0.77; our $VERSION = version->declare( 'v0.0.1' );
 use autodie;
@@ -24,12 +37,45 @@ Readonly my $GDX_BUNDLED_LOC	=> 'com/badlogic/gdx';
 Readonly my $GDX_VERSION_FILE	=> 'Version.class';
 Readonly my $GDX_NATIVE_LOC	=> '/usr/local/share/libgdx';
 
+=item add_classpath()
+
+Method for adding to the classpath.
+No-op by default.
+
+=cut
+
 sub add_classpath ( $ ) {
 	# no-op
 }
+
+=item setup($mode_obj)
+
+Setup for the a module used by L<IndieRunner::Engine::Java>.
+This is called for each used module from L<IndieRunner::Engine::Java>'s own setup method.
+No-op by default.
+
+=cut
 
 sub setup ( $, $mode_obj ) {
 	# no-op
 }
 
 1;
+
+__END__
+
+=back
+
+=head1 AUTHOR
+
+Thomas Frohwein E<lt>thfr@cpan.orgE<gt>.
+
+=head1 SEE ALSO
+
+L<IndieRunner::Engine::Java>
+
+=head1 COPYRIGHT
+
+Copyright 2022-2025 by Thomas Frohwein E<lt>thfr@cpan.orgE<gt>.
+
+This program is free software; you can redistribute it and/or modify it under the ISC license.
